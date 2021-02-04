f7a22538c691eb1ce72bb7a2544008087f2a2889
hsolbrig <solbrig@earthlink.net>
2021-02-04 08:51:53 -0600

    Add YAML, JSON and RDF emitters for test_obo_sample

--- test/test_basics.py
+++ test/test_basics.py
@@ -31,12 +31,25 @@ class BasicsTestCase(unittest.TestCase):
                               designation="TSCYC - Being Frightened of Men",
                               definition="Trauma Symptom Checklist for Young Children (TSCYC) Please indicate how often"
                                          " the child has done, felt, or experienced each of the following things in the "
-                                         "last month: Being frightened of men.", narrower_than=OBO.NCIT_C147557)
-        c1 = ConceptSystem(OBO, "OBO", contents=[e1])
+                                         "last month: Being frightened of men.", narrower_than=OBO.NCIT_C147557, reference=OBO['NCI_C147796'])
+        e2 = ConceptReference(OBO['NCI_C147557'], code="C147557", defined_in=OBO,
+                              designation="TSCYC Questionnaire Question",
+                              definition="A question associated with the TSCYC questionnaire.", narrower_than=OBO['NCI_C91102'])
+        c1 = ConceptSystem(OBO, "OBO", contents=[e1, e2])
         p1 = Package({OBO:c1})
         g = as_rdf(p1, contexts=os.path.abspath('../jsonld-context/termci_schema.context.json'))
+        self.hdr('YAML')
+        print(as_yaml(p1))
+        self.hdr('JSON')
+        print(as_json(as_json_object(p1)))
+        self.hdr('RDF')
         print(g.serialize(format='turtle').decode())
         print(g.serialize(format='json-ld').decode())
+
+    def hdr(self, txt):
+        print('-'*20, end='')
+        print(f' {txt} ', end='')
+        print('-'*20)
 
 if __name__ == '__main__':
     unittest.main()
